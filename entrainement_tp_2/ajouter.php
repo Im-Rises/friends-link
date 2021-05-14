9fe019be3f14f262b1edafd7b2c942032d0abda6
dorinebrun <dorine.brun@esme.fr>
2021-05-14 12:20:18 +0200

    dao.php

--- entrainement_tp_2/ajouter.php
+++ entrainement_tp_2/ajouter.php
@@ -20,44 +20,6 @@
 
     <?php
 
-    function insertIntoLivre($titre, $auteur)
-    {
-        global $connexion;
-
-        $titre = htmlspecialchars($_POST["titre"]);
-        $auteur = htmlspecialchars($_POST["auteur"]);
-        $req = "SELECT * FROM auteur;";
-        $array = mysqli_query($connexion, $req);
-
-        $isAuthorExist = false;
-        $idAuteur = 0;
-
-        foreach ($array as $value) {
-            if ($value["nom"] == $auteur) {
-                $isAuthorExist = true;
-                $idAuteur = $value["idAuteur"];
-                break;
-            }
-        }
-        if (!$isAuthorExist) {
-            $req = "INSERT INTO auteur(nom) VALUES ('$auteur');";
-            $res = mysqli_query($connexion, $req);
-            if (!$res) echo mysqli_errno($connexion) . ": " . mysqli_error($connexion) . "\n";
-
-            $req = "SELECT * FROM auteur WHERE nom='$auteur'";
-            $res = mysqli_query($connexion, $req);
-            if (!$res) echo mysqli_errno($connexion) . ": " . mysqli_error($connexion) . "\n";
-            foreach ($res as $value)
-                $idAuteur = $value['idAuteur'];
-        }
-
-        $req = "INSERT INTO livre(titre, idAuteur) VALUES ('$titre', '$idAuteur');";
-
-        $res = mysqli_query($connexion, $req);
-        if (!$res) echo mysqli_errno($connexion) . ": " . mysqli_error($connexion) . "\n";
-        return $res;
-    }
-
     if (isset($_POST["titre"], $_POST["auteur"]) &&  $_POST["titre"] != NULL and $_POST["auteur"] != NULL) {
 
         if (insertIntoLivre($_POST["titre"], $_POST["auteur"])) {
--- entrainement_tp_2/dao.php
+++ entrainement_tp_2/dao.php
@@ -1,8 +1,57 @@
-<?php 
+<?php
 $serveur     = "localhost";
 $utilisateur = "root";
 $mdp         = "";
 $db          = "gestion_livre";
 $connexion = mysqli_connect($serveur, $utilisateur, $mdp, $db);
 
-?>
+function selectAllAuteur()
+{
+    global $connexion;
+    $req = "SELECT * FROM auteur;";
+    return mysqli_query($connexion, $req);
+}
+
+
+function insertIntoLivre($titre, $auteur)
+{
+    global $connexion;
+
+    $titre = htmlspecialchars($_POST["titre"]);
+    $auteur = htmlspecialchars($_POST["auteur"]);
+
+    $idAuteur = insertIntoAuteur($auteur);
+    $req = "INSERT INTO livre(titre, idAuteur) VALUES ('$titre', '$idAuteur');";
+
+    $res = mysqli_query($connexion, $req);
+    if (!$res) echo mysqli_errno($connexion) . ": " . mysqli_error($connexion) . "\n";
+    return $res;
+}
+
+function insertIntoAuteur($auteur)
+
+{
+    global $connexion;
+    $isAuthorExist = false;
+    $idAuteur = 0;
+
+    foreach (selectAllAuteur() as $value) {
+        if ($value["nom"] == $auteur) {
+            $isAuthorExist = true;
+            $idAuteur = $value["idAuteur"];
+            break;
+        }
+    }
+    if (!$isAuthorExist) {
+        $req = "INSERT INTO auteur(nom) VALUES ('$auteur');";
+        $res = mysqli_query($connexion, $req);
+        if (!$res) echo mysqli_errno($connexion) . ": " . mysqli_error($connexion) . "\n";
+
+        $req = "SELECT * FROM auteur WHERE nom='$auteur'";
+        $res = mysqli_query($connexion, $req);
+        if (!$res) echo mysqli_errno($connexion) . ": " . mysqli_error($connexion) . "\n";
+        foreach ($res as $value)
+            $idAuteur = $value['idAuteur'];
+    }
+    return $idAuteur;
+}
