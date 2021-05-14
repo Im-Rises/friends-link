38ad0a1e763615d1cf396283a778fd6e3e98ead0
clementreiffers <reiffersclement@gmail.com>
2021-05-14 11:20:40 +0200

    ajout d'un fichier 'dao.php'

---
+++ entrainement_tp_2/dao.php
@@ -0,0 +1,8 @@
+<?php 
+$serveur     = "localhost";
+$utilisateur = "root";
+$mdp         = "";
+$db          = "gestion_livre";
+$connexion = mysqli_connect($serveur, $utilisateur, $mdp, $db);
+
+?>
