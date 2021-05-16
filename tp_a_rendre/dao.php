a611f4b124dee4775a5ab68dbe5eef3913e3ece8
clementreiffers <reiffersclement@gmail.com>
2021-05-16 21:47:13 +0200

    separation des requetes sql entre q et c

--- tp_a_rendre/dao.php
+++ tp_a_rendre/dao.php
@@ -8,11 +8,15 @@ $connexion = mysqli_connect($serveur, $utilisateur, $mdp, $db);
 
 /*
 fonctions necessaires en php :
+
+clement :
 -   recuperer donnes membres en fonction email
 -   recuperer tous les messages en fonction des deux emails
 -   recuprer liste amis 
--   recuprer toutes les discussions par email
--   recuperer tous les groupes par email
+
+quentin :
+-   recuperer toutes les discussions par email 
+-   recuperer tous les groupes par email 
 -   insertion des donnees
 -   recuperer les messages appartenant a un groupe
 */
