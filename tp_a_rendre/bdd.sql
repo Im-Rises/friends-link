183217f6debdfb88f83292d34b331ad348e8a3eb
clementreiffers <reiffersclement@gmail.com>
2021-05-16 21:25:47 +0200

    ajout de bdd.sql

--- tp_a_rendre/bdd.sql
+++ tp_a_rendre/bdd.sql
@@ -1,28 +1,13 @@
-CREATE DATABASE reseau_social;
+DROP DATABASE IF EXISTS reseau_social;
+CREATE DATABASE IF NOT EXISTS reseau_social;
 USE reseau_social;
 
-CREATE TABLE membres (
-    email TEXT PRIMARY KEY, 
-    nom TEXT, 
-    prenom TEXT, 
-    date_naissance DATE
-);
+CREATE TABLE membre (adresse_mail VARCHAR(320) PRIMARY KEY, nom VARCHAR(50) NOT NULL , prenom VARCHAR(50) NOT NULL, date_naissance DATE);
 
-CREATE TABLE discussions (
-    email1 TEXT PRIMARY KEY,
-    email2 TEXT PRIMARY KEY,
-    FOREIGN KEY(email1) REFERENCES membres(email),
-    FOREIGN KEY(email2) REFERENCES membres(email)
-);
+CREATE TABLE message_discussion (id_message BIGINT PRIMARY KEY AUTO_INCREMENT, email_envoyeur VARCHAR(320) NOT NULL, email_receveur VARCHAR(320) NOT NULL, message_text VARCHAR(1000) NOT NULL, date_envoie DATE NOT NULL, FOREIGN KEY (email_envoyeur) REFERENCES membre(adresse_mail), FOREIGN KEY (email_receveur) REFERENCES membre(adresse_mail));
 
-CREATE TABLE groupe (
-    id INT PRIMARY KEY,
-    nom TEXT
-);
+CREATE TABLE ami(email VARCHAR(320), email_ami VARCHAR(320), amitie_validee BOOLEAN NOT NULL, PRIMARY KEY (email, email_ami), FOREIGN KEY (email) REFERENCES membre(adresse_mail), FOREIGN KEY (email) REFERENCES membre(adresse_mail));
+CREATE TABLE groupe (id BIGINT PRIMARY KEY, nom VARCHAR(25) NOT NULL);
+CREATE TABLE groupe_membre (id_groupe BIGINT, mail_membre VARCHAR(320), date_creation DATE, PRIMARY KEY(id_groupe, mail_membre), FOREIGN KEY (id_groupe) REFERENCES groupe(id), FOREIGN KEY (mail_membre) REFERENCES membre(adresse_mail));
 
-CREATE TABLE message_groupe (
-    email_envoyeur TEXT PRIMARY KEY,
-    id_groupe INT PRIMARY KEY,
-    msg TEXT, 
-    date_msg DATE
-);
+CREATE TABLE message_groupe (id_message BIGINT PRIMARY KEY AUTO_INCREMENT, email_envoyeur VARCHAR(320), id_groupe BIGINT, text_message VARCHAR(1000) NOT NULL, date_envoie DATE, FOREIGN KEY (email_envoyeur) REFERENCES membre(adresse_mail), FOREIGN KEY (id_groupe) REFERENCES groupe(id));
