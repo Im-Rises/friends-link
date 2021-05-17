67a8b4b939e87c0bb97678242e2fc36f4327d723
clementreiffers <reiffersclement@gmail.com>
2021-05-17 10:51:04 +0200

    login et register fonctionnels, bdd modifie, l'adresse mail d'un membre ne peut être nulle

--- tp_a_rendre/login.php
+++ tp_a_rendre/login.php
@@ -1,84 +1,49 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>FriendsLink</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+</head>
+
+<body>
+
+
+    <main>
+        <article style="margin:10px 50px 0px;">
+            <div style="padding: 20px 20px 30px; margin: 20px 0px 30px; width: 25rem;  background: #e7e7e7;">
+                <h3>Connexion:</h3>
+                <form method="post">
+                    <input type="mail" name="mail" placeholder="Email">
+                    <input type="password" name="password" placeholder="Mot de passe">
+                    <input type="submit" value="Envoyer" style="margin: 30px 5px 0px;">
+                </form>
+
+            </div>
+        </article>
+    </main>
+</body>
+
+</html>
 <?php
 session_start();
 
-//Cette condition permet de savoir si une session est déjà ouverte sur ce nivagateur
-if(isset($_SESSION["login"])){
-    header("Location: ./accueil.php"); 
-}
-//Si l'utilisateur est déjà connecté, on le redirige vers la page d'accueil
+require "dao.php";
 
-/*On met le bout de code qui sert à se connecter à la bdd dans un fichier à part 
-(Pour éviter d'avoir à le retaper à chaque fois)*/
-require_once('scripts/bdd.php');
+if (isset($_POST["mail"], $_POST["password"]) and $_POST["password"] != NULL and $_POST["mail"] != NULL) {
 
-//Cette condition permet de savoir si le formulaire a été soumis
-if(isset($_POST["toto"])){
+    $mail = htmlspecialchars($_POST['mail']);
 
-    /*Rappel : On manipule la bdd avec les méthodes de la classe PDO.
-    Pour comprendre : https://www.php.net/manual/fr/pdo.prepare.php*/
+    $membre = selectDataMembersWhereEmail($mail);
 
-    $mail = $_POST['mail'];
-    
-    //On prépare la requête pour éviter les injections. Pour comprendre: https://www.journaldunet.fr/web-tech/developpement/1202885-comment-eviter-une-injection-sql-en-php/#:~:text=Pour%20pr%C3%A9venir%20les%20injections%20SQL,code%20SQL%20dans%20des%20champs.
-    $req = $bdd->prepare("SELECT  `id`, `email`, `password`
-    FROM  `user`
-    WHERE `email` = '$mail'");
+    $membre = mysqli_fetch_array($membre);
 
-    // Ici on éxécute la requête préparée ci-dessus
-    $req->execute();
-
-    // Ici on stocke le résultat de la requête dans un tableau
-    $reponse = $req->fetch();
-
-    /* Cette condition permet de décrypter le mot de passe et de vérifier si il correspond.
-     Pour comprendre: https://www.php.net/manual/fr/function.password-verify.php */
-    if (password_verify($_POST['password'], $reponse[2])) {
-        $_SESSION['login'] = $reponse[0];
-        header("Location: ./accueil.php");
+    if (password_verify($_POST['password'], $membre["mdp"])) {
+        $_SESSION["email"] = $membre["adresse_mail"];
+        echo "c'est bon !";
     } else {
-	echo 'Le mot de passe est invalide! Veuillez réesayer';
+        echo 'Le mot de passe est invalide! Veuillez réesayer';
     }
-
-    
 }
 ?>
-
-<!DOCTYPE HTML>
-<html>
-	<head>  
-		<title>FriendsLink</title>
-		<meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-    </head>
-    
-    <body>
-
-        <header>
-            <nav>
-		<!--Plus tard mettre une navbar.-->
-            </nav>
-        </header>  
-
-        <main>
-            <article style="margin:10px 50px 0px;">
-                <div style="padding: 20px 20px 30px; margin: 20px 0px 30px; width: 25rem;  background: #e7e7e7;">
-                <h3>Connexion:</h3>
-                <form method="post">
-                    <div>
-                        <label for="inputEmail3">Mail:</label>
-                        <input type="mail" name="mail" placeholder="Email">
-                    </div>
-                    <div>
-                        <label for="form">Mot de passe:</label>
-                        <input type="password" name="password" placeholder="Mot de passe">
-                    </div>
-                    <input type="submit" value="Envoyer" style="margin: 30px 5px 0px;">
-                    
-                </form>
-                
-            </div>
-            </article>
-        </main>
-	</body>
-
-</html>
--- tp_a_rendre/register.php
+++ tp_a_rendre/register.php
@@ -1,87 +1,52 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>FriendsLink</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+</head>
+
+<body>
+
+    <main>
+        <article style="margin:10px 50px 0px;">
+            <div style="padding: 20px 20px 30px; margin: 20px 0px 30px; width: 25rem;  background: #e7e7e7;">
+                <h3>Créer un compte:</h3>
+                <form method="post">
+                    <input type="text" name="nom" placeholder="Nom">
+                    <input type="text" name="prenom" placeholder="Prenom">
+                    <input type="mail" name="email" placeholder="Email">
+                    <input type="password" name="password" placeholder="Mot de passe">
+                    <input type="date" name="bday">
+                    <input type="submit" value="Envoyer" style="margin: 30px 5px 0px;">
+                </form>
+
+            </div>
+        </article>
+    </main>
+</body>
+
+</html>
 <?php
 session_start();
-
 //Cette condition permet de savoir si une session est déjà ouverte sur ce nivagateur
-if(isset($_SESSION["login"])){
-    header("Location: ./accueil.php"); 
+if (isset($_SESSION["login"])) {
+    header("Location: ./accueil.php");
 }
-//Si l'utilisateur est déjà connecté, on le redirige vers la page d'accueil
-
-/*On met le bout de code qui sert à se connecter à la bdd dans un fichier à part 
-(Pour éviter d'avoir à le retaper à chaque fois)*/
-require_once('scripts/bdd.php');
 
 //Cette condition permet de savoir si le formulaire a été soumis
-if(isset($_POST["nom"])){ 
+if (isset($_POST["nom"], $_POST["prenom"], $_POST["bday"], $_POST["email"], $_POST["password"]) 
+    and $_POST["nom"] != NULL and $_POST["prenom"] != NULL and $_POST["email"] != NULL 
+    and $_POST["password"] != NULL and $_POST["bday"] != NULL) {
 
-    /*Rappel : On manipule la bdd avec les méthodes de la classe PDO.
-    Pour comprendre : https://www.php.net/manual/fr/pdo.prepare.php*/
+    require "dao.php";
 
-    $requete = $bdd->prepare('INSERT INTO `user`(`nom`, `prenom`, `droit`, `email`, `password`, `id_Ets`) 
-    VALUES (:nom,:prenom, :email, :password)');
-
-    /*On ne stock pas les mdp en clair dans la bdd pour cela on utilise:
-    https://www.php.net/manual/fr/function.password-hash.php*/
     $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
-    
-    $requete->execute(array(
-        ':nom'=> $_POST["nom"],
-        ':prenom'=> $_POST["prenom"],
-        ':email'=> $_POST["mail"],
-        ':password'=> $password,
-    )); 
-    $requete->closeCursor();
 
-    //Une fois le compte créé, on redirige vers la page login
+    insertIntoMembre($_POST["email"], $_POST["nom"], $_POST["prenom"], $_POST["bday"], $password);
+
     header('Location: ./login.php');
 }
 
 ?>
-
-<!DOCTYPE HTML>
-<html>
-	<head>  
-		<title>FriendsLink</title>
-		<meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-    </head>
-    
-    <body>
-
-        <header>
-            <nav>
-		<!--Plus tard mettre une navbar.-->
-            </nav>
-        </header>  
-
-        <main>
-            <article style="margin:10px 50px 0px;">
-                <div style="padding: 20px 20px 30px; margin: 20px 0px 30px; width: 25rem;  background: #e7e7e7;">
-                <h3>Créer un compte:</h3>
-                <form method="post">
-		    <div>
-                        <label>Nom:</label>
-                        <input type="text" name="nom" placeholder="Nom">
-                    </div>
-		    <div>
-                        <label>Prenom:</label>
-                        <input type="text" name="prenom" placeholder="Prenom">
-                    </div>
-                    <div>
-                        <label for="inputEmail3">Mail:</label>
-                        <input type="mail" name="email" placeholder="Email">
-                    </div>
-                    <div>
-                        <label for="form">Mot de passe:</label>
-                        <input type="password" name="password" placeholder="Mot de passe">
-                    </div>
-                    <input type="submit" value="Envoyer" style="margin: 30px 5px 0px;">
-                    
-                </form>
-                
-            </div>
-            </article>
-        </main>
-	</body>
-
-</html>
