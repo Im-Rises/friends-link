be08bd4319a5fc76fe3315ee467770325cb45d5a
clementreiffers <reiffersclement@gmail.com>
2021-05-17 10:51:40 +0200

    bdd.php delete

--- tp_a_rendre/bdd.php
+++
@@ -1,5 +0,0 @@
-<?php
-	//Ici on créer une nouvelle instance de la classe PDO qui nous permettra de manipuler la BDD plus facilement
-        $bdd = new PDO('mysql:host=localhost;dbname=friendsLink;charset=utf8', 'root','');
-        $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
