7345c11dc046a6e63ab113e9e3a8c8cdc4db5cf1
Im-Rises <quentin-pimbole@hotmail.fr>
2021-05-19 12:45:14 +0200

    Correction bug div

--- tp_a_rendre/friendsRequest.php
+++ tp_a_rendre/friendsRequest.php
@@ -20,25 +20,27 @@ if (isset($_SESSION["email"]) and $_SESSION["email"] != NULL) {
                 <div class='headElement'>Prenom</div>
                 <div class='headElement'>Email</div>
             </div>
+
             <?php
             $res = selectProfilsReceptionDemandeAmi($_SESSION["email"]);
             foreach ($res as $emails) {
                 echo "
-            <br>
-            <div class='divBody'>
-                <div class='bodyElement'>$emails[prenom]</div>
-                <div class='bodyElement'>$emails[nom]</div>
-                <div class='bodyElement'>$emails[adresse_mail]</div>
-            </div>";
+                    <br>
+                    <div class='divBody'>
+                        <div class='bodyElement'>$emails[prenom]</div>
+                        <div class='bodyElement'>$emails[nom]</div>
+                        <div class='bodyElement'>$emails[adresse_mail]</div>
+                    </div>
+            ";
             }
             ?>
-        </div>
+
 
 
         </br>
 
 
-        <h1>Liste des demandes d'amis envoyées :</h1>
+        <!-- <h1>Liste des demandes d'amis envoyées :</h1>
         <div class='showTab'>
             <div class='divHead'>
                 <div class='headElement'>Nom</div>
@@ -46,23 +48,23 @@ if (isset($_SESSION["email"]) and $_SESSION["email"] != NULL) {
                 <div class='headElement'>Email</div>
             </div>
             <?php
-            $res = selectProfilsDemandesEnAmi($_SESSION["email"]);
-            foreach ($res as $emails) {
-                echo "
-            <br>
-            <div class='divBody'>
-                <div class='bodyElement'>$emails[prenom]</div>
-                <div class='bodyElement'>$emails[nom]</div>
-                <div class='bodyElement'>$emails[adresse_mail]</div>
-            </div>";
-            }
+            // $res = selectProfilsDemandesEnAmi($_SESSION["email"]);
+            // foreach ($res as $emails) {
+            //     echo "
+            // <br>
+            // <div class='divBody'>
+            //     <div class='bodyElement'>$emails[prenom]</div>
+            //     <div class='bodyElement'>$emails[nom]</div>
+            //     <div class='bodyElement'>$emails[adresse_mail]</div>
+            // </div>";
+            // }
             ?>
-        </div>
+        </div> -->
 
     </body>
 
     </html>
-    
+
 <?php } else {
     header("Location: login.php");
 }
