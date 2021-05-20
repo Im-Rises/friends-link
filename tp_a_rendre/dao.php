3889cd80047f8931e27a65555a0ef8e66edd6f99
clementreiffers <reiffersclement@gmail.com>
2021-05-20 08:21:32 +0200

    centralisation de la protection des variables par une fonction dans le dao

--- tp_a_rendre/dao.php
+++ tp_a_rendre/dao.php
@@ -6,26 +6,28 @@ $db          = "reseau_social";
 $connexion = mysqli_connect($serveur, $utilisateur, $mdp, $db);
 
 
+function protection($val)
+{
+    $val = htmlspecialchars($val);
+    $val = htmlentities($val);
+    return $val;
+}
+
 // INSERT
 
 function insertIntoMembre($email, $nom, $prenom, $bday, $mdp) // works
 {
     global $connexion;
 
-    $email = htmlspecialchars($email); // protege des injections de code html ou js
-    $email = htmlentities($email); // protege des injections sql
+    $email = protection($email);
 
-    $nom = htmlspecialchars($nom); // protege des injections de code html ou js
-    $nom = htmlentities($nom); // protege des injections sql
+    $nom = protection($nom);
 
-    $prenom = htmlspecialchars($prenom); // protege des injections de code html ou js
-    $prenom = htmlentities($prenom); // protege des injections sql
+    $prenom = protection($prenom); // protege des injections sql
 
-    $bday = htmlspecialchars($bday); // protege des injections de code html ou js
-    $bday = htmlentities($bday); // protege des injections sql
+    $bday = protection($bday); // protege des injections sql
 
-    $mdp = htmlspecialchars($mdp); // protege des injections de code html ou js
-    $mdp = htmlentities($mdp); // protege des injections sql
+    $mdp = protection($mdp); // protege des injections sql
     $mdp = password_hash($mdp, PASSWORD_DEFAULT);
 
     $req = "INSERT INTO membre(adresse_mail, nom, prenom, date_naissance, mdp) VALUES ('$email', '$nom', '$prenom', '$bday', '$mdp');";
@@ -37,14 +39,11 @@ function insertIntoMessageDiscussion($sender, $receiver, $msg) // works
 {
     global $connexion;
 
-    $sender = htmlspecialchars($sender); // protege des injections de code html ou js
-    $sender = htmlentities($sender); // protege des injections sql
+    $sender = protection($sender); // protege des injections sql
 
-    $receiver = htmlspecialchars($receiver); // protege des injections de code html ou js
-    $receiver = htmlentities($receiver); // protege des injections sql
+    $receiver = protection($receiver); // protege des injections sql
 
-    $msg = htmlspecialchars($msg); // protege des injections de code html ou js
-    $msg = htmlentities($msg); // protege des injections sql
+    $msg = protection($msg); // protege des injections sql
 
     $req = "INSERT INTO message_discussion(email_envoyeur, email_receveur, message_text, date_envoie) VALUES ('$sender', '$receiver', '$msg', CURRENT_DATE());";
 
@@ -57,8 +56,7 @@ function selectMembreWhereEmail($email)
 {
     global $connexion;
 
-    $email = htmlspecialchars($email); // protege des injections de code html ou js
-    $email = htmlentities($email); // protege des injections sql
+    $email = protection($email); // protege des injections sql
 
     $req = "SELECT * FROM membre WHERE adresse_mail = '$email';";
 
@@ -71,11 +69,9 @@ function insertIntoAmi($email, $email_ami, $amitie_validee) // works
 {
     global $connexion;
 
-    $email = htmlspecialchars($email); // protege des injections de code html ou js
-    $email = htmlentities($email); // protege des injections sql
+    $email = protection($email); // protege des injections sql
 
-    $email_ami = htmlspecialchars($email_ami); // protege des injections de code html ou js
-    $email_ami = htmlentities($email_ami); // protege des injections sql
+    $email_ami = protection($email_ami); // protege des injections sql
 
     $req = "INSERT INTO Ami VALUES ('$email', '$email_ami', '$amitie_validee', NOW())";
 
@@ -88,8 +84,7 @@ function insertIntoGroupe($nom) // works
 {
     global $connexion;
 
-    $nom = htmlspecialchars($nom); // protege des injections de code html ou js
-    $nom = htmlentities($nom); // protege des injections sql
+    $nom = protection($nom); // protege des injections sql
 
     $req = "INSERT INTO Groupe VALUES (NULL, '$nom')";
 
@@ -102,11 +97,9 @@ function insertIntoGroupeMembre($id_groupe, $email_membre)
 {
     global $connexion;
 
-    $id_groupe = htmlspecialchars($id_groupe); // protege des injections de code html ou js
-    $id_groupe = htmlentities($id_groupe); // protege des injections sql
+    $id_groupe = protection($id_groupe); // protege des injections sql
 
-    $email_membre = htmlspecialchars($email_membre); // protege des injections de code html ou js
-    $email_membre = htmlentities($email_membre); // protege des injections sql
+    $email_membre = protection($email_membre); // protege des injections sql
 
     $req = "INSERT INTO groupe_membre VALUES('$id_groupe', '$email_membre',NOW())";
 
@@ -119,14 +112,11 @@ function insertIntoMessageGroupe($email_envoyeur, $id_groupe, $message)
 {
     global $connexion;
 
-    $email_envoyeur = htmlspecialchars($email_envoyeur); // protege des injections de code html ou js
-    $email_envoyeur = htmlentities($email_envoyeur); // protege des injections sql
+    $email_envoyeur = protection($email_envoyeur); // protege des injections sql
 
-    $id_groupe = htmlspecialchars($id_groupe); // protege des injections de code html ou js
-    $id_groupe = htmlentities($id_groupe); // protege des injections sql
+    $id_groupe = protection($id_groupe); // protege des injections sql
 
-    $message = htmlspecialchars($message); // protege des injections de code html ou js
-    $message = htmlentities($message); // protege des injections sql
+    $message = protection($message); // protege des injections sql
 
     $req = "INSERT INTO message_groupe VALUES('$email_envoyeur','$id_groupe','$message', NOW());";
 
@@ -142,8 +132,7 @@ function selectDataMembersWhereEmail($email)
 {
     global $connexion;
 
-    $email = htmlspecialchars($email); // protege des injections de code html ou js
-    $email = htmlentities($email); // protege des injections sql
+    $email = protection($email); // protege des injections sql
 
     $req = "SELECT * FROM membre WHERE adresse_mail = '$email';";
 
@@ -157,11 +146,9 @@ function selectMessagesWithTwoEmail($email1, $email2)
 {
     global $connexion;
 
-    $email1 = htmlspecialchars($email1); // protege des injections de code html ou js
-    $email1 = htmlentities($email1); // protege des injections sql
+    $email1 = protection($email1); // protege des injections sql
 
-    $email2 = htmlspecialchars($email2); // protege des injections de code html ou js
-    $email2 = htmlentities($email2); // protege des injections sql
+    $email2 = protection($email2); // protege des injections sql
 
     $req = "SELECT  *
             FROM message_discussion
@@ -183,8 +170,7 @@ function selectAllFriendsWhereEmail($email)
 {
     global $connexion;
 
-    $email = htmlspecialchars($email); // protege des injections de code html ou js
-    $email = htmlentities($email); // protege des injections sql
+    $email = protection($email); // protege des injections sql
 
     $req = "SELECT * FROM membre m JOIN ami a ON a.email_ami = m.adresse_mail WHERE a.email='$email' AND a.amitie_validee=1;";
 
@@ -197,8 +183,7 @@ function selectAllMembersWhereNomPrenomEmailWhereSearch($search, $email)
 {
     global $connexion;
 
-    $search = htmlspecialchars($search); // protege des injections de code html ou js
-    $search = htmlentities($search); // protege des injections sql
+    $search = protection($search); // protege des injections sql
 
     $req = "SELECT DISTINCT *
             FROM membre m
@@ -218,8 +203,7 @@ function selectEmailsDiscussion($email)
 {
     global $connexion;
 
-    $email = htmlspecialchars($email);
-    $email = htmlentities($email);
+    $email = protection($email);
 
     $req = "SELECT * FROM membre m JOIN message_discussion md ON md.email_receveur = m.adresse_mail WHERE md.email_receveur = '$email';";
 
@@ -232,11 +216,9 @@ function selectMessagesDiscussion($email1, $email2)
 {
     global $connexion;
 
-    $email1 = htmlspecialchars($email1);
-    $email1 = htmlentities($email1);
+    $email1 = protection($email1);
 
-    $email2 = htmlspecialchars($email2);
-    $email2 = htmlentities($email2);
+    $email2 = protection($email2);
 
     $req = "SELECT * FROM message_discussion WHERE email_envoyeur='$email1' AND email_receveur='$email2' UNION SELECT * FROM message_discussion WHERE email_envoyeur='$email2' AND email_receveur='$email1' ORDER BY id_message;";
 
@@ -251,14 +233,20 @@ function selectAllGroupes($email)
 {
     global $connexion;
 
-    $email = htmlspecialchars($email);
-    $email = htmlentities($email);
+    $email = protection($email);
 
-    $req = "SELECT nom FROM groupe g JOIN groupe_membre gm ON gm.id_groupe = g.id WHERE mail_membre = '$email';";   
+    $req = "SELECT nom FROM groupe g JOIN groupe_membre gm ON gm.id_groupe = g.id WHERE mail_membre = '$email';";
 
     $res = mysqli_query($connexion, $req);
     if (!$res) echo mysqli_errno($connexion) . ": " . mysqli_error($connexion) . "\n";
     return $res;
+}
+
+function selectAllMessagesFromGroupeWhereId($id)
+{
+    global $connexion;
+
+    $id = protection($id);
 }
 
 
@@ -289,8 +277,7 @@ function selectDemandesAmi($email)
 {
     global $connexion;
 
-    $email = htmlspecialchars($email);
-    $email = htmlentities($email);
+    $email = protection($email);
 
     $req = "SELECT email_ami FROM ami WHERE email='$email' AND amitie_validee=false";
 
@@ -357,8 +344,7 @@ function selectProfilsReceptionDemandeAmi($email)
 {
     global $connexion;
 
-    $email = htmlspecialchars($email);
-    $email = htmlentities($email);
+    $email = protection($email);
 
     $req = "SELECT * FROM membre WHERE adresse_mail IN (SELECT email FROM ami WHERE email_ami='$email' AND amitie_validee=0);";
 
@@ -373,19 +359,16 @@ function creerAmitie($emailDemandeur, $emailAccepteur)
 {
     global $connexion;
 
-    $emailDemandeur = htmlspecialchars($emailDemandeur);
-    $emailDemandeur = htmlentities($emailDemandeur);
+    $emailDemandeur = protection($emailDemandeur);
 
-    $emailAccepteur = htmlspecialchars($emailAccepteur);
-    $emailAccepteur = htmlentities($emailAccepteur);
+    $emailAccepteur = protection($emailAccepteur);
 
     $req = "UPDATE ami SET amitie_validee=1 WHERE email='$emailDemandeur' AND email_ami='$emailAccepteur';";
 
     $res = mysqli_query($connexion, $req);
     if (!$res) {
         echo mysqli_errno($connexion) . ": " . mysqli_error($connexion) . "\n";
-    }
-    else {
+    } else {
         insertIntoAmi($emailAccepteur, $emailDemandeur, 1);
     }
 
---
+++ tp_a_rendre/messages_groupe.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Groupe</title>
+</head>
+
+<?php require "dao.php"; ?>
+
+<body>
+    <?php 
+    
+    
+    
+    ?>
+</body>
+
+</html>
