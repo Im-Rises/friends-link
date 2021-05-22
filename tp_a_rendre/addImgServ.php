292189a3dcf022ef0295c29ede1075afe8b4433d
Im-Rises <quentin-pimbole@hotmail.fr>
2021-05-22 15:53:15 +0200

    Ajout d'une page pour envoyer des image sur le serveur

---
+++ tp_a_rendre/addImgServ.php
@@ -0,0 +1,37 @@
+<?php include "ban.php";
+if (isset($_SESSION["email"]) and $_SESSION["email"] != NULL) {
+?>
+
+    <!DOCTYPE html>
+    <html>
+
+    <body>
+
+        <form action="" method="post" enctype="multipart/form-data">
+            <label for="fileToUpload">Sélectionnez une image depuis votre appareil :</label></br>
+            <input type="file" name="fileToUpload" accept="image/*"></br>
+            <input type="submit" value="Envoyer"></br>
+        </form>
+
+        <?php
+        //var_dump($_FILES);
+
+        echo $_SESSION['email'];
+        $imageName=$_SESSION['email'];
+        //$imageName = "testToutout";
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            move_uploaded_file($_FILES['fileToUpload']['tmp_name'], "images/$imageName");
+        }
+
+        ?>
+
+    </body>
+
+    </html>
+
+<?php
+} else {
+    header("Location: login.php");
+}
+?>
