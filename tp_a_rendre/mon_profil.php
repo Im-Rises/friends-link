74b78056830feae6bca378c7e30ed0666770e962
dorinebrun <dorine.brun@esme.fr>
2021-06-03 00:41:29 +0200

    css mon_profil

--- tp_a_rendre/mon_profil.php
+++ tp_a_rendre/mon_profil.php
@@ -8,87 +8,101 @@ if (isset($_SESSION["email"]) and $_SESSION["email"] != NULL) {
 ?>
 
     <!DOCTYPE html>
-    <html lang="fr">
+    <html>
 
     <head>
         <title>Mon profil</title>
         <meta charset="utf-8" />
         <link rel="stylesheet" href="mon_profil.css">
-        <link rel="stylesheet" href="indexLog.css">
-        <link rel="stylesheet" href="show_all_discussions.css">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet">
         <?php
         $css = pathinfo($_SERVER['PHP_SELF'], PATHINFO_BASENAME) == "index.php" ? "indexBan.css" : "ban.css";
         echo "<link rel='stylesheet' href='$css'>";
         ?>
     </head>
 
+
+    <?php include "ban.php"; ?>
+
     <body>
 
 
         <?php
-        include "ban.php";
         //affichage des infos de l'utilisateur connecté 
         echo "<h1>$profil[prenom] $profil[nom]</h1>";
-        echo "<a href=addImgServ.php><img src='" . recupImageEmail($profil['adresse_mail']) . "' class='pdpMonProfile' alt='image de profil'></a>";
+        echo "<a href=addImgServ.php><img src='" . recupImageEmail($profil['adresse_mail']) . "'class='pdpMonProfile' ></a>";
         ?>
 
         <p class='centerText'>Adresse mail : <?php echo $profil['adresse_mail'] ?></p>
 
-        <h2>Liste d'amis</h2>
+        <h1>Votre liste d'amis: </h1>
 
         <?php
         if (isset($_GET['voirPlusAmis']) and $_GET['voirPlusAmis'] != NULL && $_GET['voirPlusAmis'] == 'Voir') {
         ?>
             <div class="allTab">
-                <h1>Tous les amis :</h1>
-                <?php
-                $array = selectAllFriendsWhereEmail($_SESSION["email"]);
-                foreach ($array as $value) {
-                    $nom = $value["nom"];
-                    $prenom = $value["prenom"];
-                    $receiver = $value["adresse_mail"];
+                <h2>Tous vos amis :</h2>
+                <div class='showTab'>
+                    <div class='divHead'>
+                        <div class='headElement'>Image</div>
+                        <div class='headElement'>Nom</div>
+                        <div class='headElement'>Prenom</div>
+                        <div class='headElement'>Email</div>
+                        <div class='headElement'>Supprimer</div>
+                    </div>
 
-                    echo "
+
+                    <?php
+                    $listeAmi = selectAllFriendsWhereEmail($profil['adresse_mail']);
+                    //$array = selectAllFriendsWhereEmail($_SESSION["email"]);
+
+                    foreach ($listeAmi as $value) {
+                        $nom = $value["nom"];
+                        $prenom = $value["prenom"];
+                        $receiver = $value["adresse_mail"];
+
+                        echo "
+                    <br>
                     <div class='divBody'>
-                        <a href='messages.php?receiver=$receiver'><div class='pdpBodyElement'><img src='" . recupImageEmail($receiver) . "' class='pdp' alt='image de profil'></div></a>
+                        <a href='messages.php?receiver=$receiver'><div class='pdpBodyElement'><img src='" . recupImageEmail($receiver) . "' class='pdp'></div></a>
                         <a href='messages.php?receiver=$receiver'><div class='bodyElement'>$nom</div></a>
                         <a href='messages.php?receiver=$receiver'><div class='bodyElement'>$prenom</div></a>
                         <a href='messages.php?receiver=$receiver'><div class='bodyElement'>$receiver</div></a>
                         <a href='?suppression=$receiver'><div class='bodyElement'>Suppression</div></a>
                     </div>";
-                }
+                    }
 
+                    ?>
+                    <!-- </div> -->
+                    <!-- </div> -->
+                <?php
+            } else {
+                echo "<a href='?voirPlusAmis=Voir&voirPlusPost' class='centerText'>Voir amis</a>";
+            }
                 ?>
-                <!-- </div> -->
-                <!-- </div> -->
-            <?php
-        } else {
-            echo "<a href='?voirPlusAmis=Voir&voirPlusPost' class='centerText'>Voir amis</a>";
-        }
-            ?>
 
 
 
 
-            <h2>Liste des posts</h2>
+                <h2>Liste des posts</h2>
 
-            <!-- Ici faire l'affichage de tous les posts de la personne -->
+                <!-- Ici faire l'affichage de tous les posts de la personne -->
 
-            <?php
-            if (isset($_GET['voirPlusPosts']) and $_GET['voirPlusPosts'] != NULL && $_GET['voirPlusPosts'] == 'Voir') {
+                <?php
+                if (isset($_GET['voirPlusPosts']) and $_GET['voirPlusPosts'] != NULL && $_GET['voirPlusPosts'] == 'Voir') {
 
-                $listePosts = selectAllPostsFromMembreOrder($profil['adresse_mail']);
+                    $listePosts = selectAllPostsFromMembreOrder($profil['adresse_mail']);
 
-                foreach ($listePosts as $post) {
-                    $array = selectLikesWhereEmailAndId($_SESSION["email"], $post["id_post"]);
-                    $array = mysqli_fetch_array($array);
+                    foreach ($listePosts as $post) {
+                        $array = selectLikesWhereEmailAndId($_SESSION["email"], $post["id_post"]);
+                        $array = mysqli_fetch_array($array);
 
-                    $like = empty($array)
-                        ? "<a href='liker.php?id_post=$post[id_post]' class='actionPost'>Aimer</a>"
-                        : "<a href='disliker.php?id_post=$post[id_post]' class='actionPost'>Ne Plus Aimer</a>";
-                    // Afficher la liste des posts des amis ici
-                    if ($post['image_post']) {
-                        echo "
+                        $like = empty($array)
+                            ? "<a href='liker.php?id_post=$post[id_post]' class='actionPost'>Aimer</a>"
+                            : "<a href='disliker.php?id_post=$post[id_post]' class='actionPost'>Ne Plus Aimer</a>";
+                        // Afficher la liste des posts des amis ici
+                        if ($post['image_post']) {
+                            echo "
                     <article class='post'>
                         <div class='insidePost'>
                             <h1>$post[titre]</h1>
@@ -108,9 +122,9 @@ if (isset($_SESSION["email"]) and $_SESSION["email"] != NULL) {
                         </div>
     
                     </article>";
-                        // echo "</a>";
-                    } else {
-                        echo "
+                            // echo "</a>";
+                        } else {
+                            echo "
                     <article class='post'>
                         <div class='insidePost'>
                             <h1>$post[titre]</h1>
@@ -122,20 +136,20 @@ if (isset($_SESSION["email"]) and $_SESSION["email"] != NULL) {
                             </div>
                         </div>
                     </article>";
+                        }
                     }
+
+                ?>
+                    <a href='?voirPlusPosts=VoirMoins' class='centerText'>Voir moins</a>
+                <?php
+                } else {
+                    echo "<a href='?voirPlusPosts=Voir' class='centerText'>Voir posts</a>";
                 }
 
-            ?>
-                <a href='?voirPlusPosts=VoirMoins' class='centerText'>Voir moins</a>
-            <?php
-            } else {
-                echo "<a href='?voirPlusPosts=Voir' class='centerText'>Voir posts</a>";
-            }
-
-            if (isset($_GET['suppression']) and $_GET['suppression'] != NULL) {
-                deleteAmitie($_SESSION["email"], $_GET['suppression']);
-            }
-            ?>
+                if (isset($_GET['suppression']) and $_GET['suppression'] != NULL) {
+                    deleteAmitie($_SESSION["email"], $_GET['suppression']);
+                }
+                ?>
 
     </body>
 
