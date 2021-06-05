8816a8a3ac9c642448f26e29981e4fbf2dbee8d3
Im-Rises <quentin-pimbole@hotmail.fr>
2021-06-05 22:20:20 +0200

    Correction du centrage de la date d'envoi messages

--- tp_a_rendre/messages.php
+++ tp_a_rendre/messages.php
@@ -62,7 +62,7 @@
                     $msg = $value["message_text"];
                     $date = $value["date_envoie"];
                     if ($dateTemp != $date) {
-                        echo "--------- $date ---------";
+                        echo "<center>--------- $date ---------</center>";
                         $dateTemp = $date;
                     }
                     if ($emailSender == $_SESSION["email"]) {
